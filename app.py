449a8a11961f6ce33fe41f0f6e83355a6f503a67
Khoi Hoang <75056589+BlueIsntSad@users.noreply.github.com>
2022-07-17 11:29:32 +0700

    app st

--- app.py
+++ app.py
@@ -1,40 +1,157 @@
-from pyspark.rdd import RDD
-from pyspark.sql import Row
 import streamlit as st
+import numpy as np
+import pandas as pd
 from utils import _initialize_spark
-
-st.write("# :tada: Hello Pyspark")
-
-spark, sc = _initialize_spark()
-st.write("[Link to Spark window](http://localhost:4040)")
-
-st.write("## Create RDD from a Python list")
-
-l = list(range(10))
-# st.write(l)
-
-rdd = sc.parallelize(l)
-rdd.cache()
-st.write(rdd)
-
-st.write("## Get results through actions")
-st.write(rdd.collect())
-st.write(rdd.take(3))
-st.write(rdd.count())
-
-st.write("## Transform RDDs")
-st.write(rdd.filter(lambda x: x%2==0).collect())  # talk about lazy evaluation here: filter still non evaluated rdd
-st.write(rdd.map(lambda x: x*2).collect()) 
-st.write(rdd.map(lambda x: x*2).reduce(lambda x, y: x + y))  # reduce runs all previous rdds
-# Compare the two following
-st.write(rdd.map(lambda x: list(range(x))).collect()) 
-st.write(rdd.flatMap(lambda x: list(range(x))).collect()) 
-
-st.write("## Wordcount")
-file_rdd = sc.textFile("lorem.txt")
-st.write(file_rdd.collect())  # so what's inside ?
-st.write(file_rdd.flatMap(lambda sentence: sentence.split()).map(lambda word: (word, 1)).reduceByKey(lambda x,y: x+y).collect())
-st.write()
+from pyspark.sql.types import *
+import pyspark.sql.functions as f
+from pyspark.sql.functions import udf, col
+from pyspark.ml.regression import LinearRegressionModel, RandomForestRegressionModel, GBTRegressionModel, DecisionTreeRegressionModel, IsotonicRegressionModel, FMRegressionModel
+from pyspark.ml.feature import VectorAssembler, StandardScaler
+from pyspark.ml.evaluation import RegressionEvaluator
+import plotly.express as px
 
 
-# spark.stop()
+def tranformFetures(X, assembler):
+    # Tạo bản sao để tránh ảnh hưởng dữ liệu gốc
+    X_ = X.copy()
+    ###########################
+
+
+
+    ###########################
+    st.write("tranform")
+    return X_tranform
+
+def prediction(samples, model):
+    st.write("predict")
+    # Encode dữ liệu
+    X = tranformFetures(samples, assembler)
+    # Predict
+    return model.predict(X)
+
+def model_page(model_name, model):
+    option_list = ['Dữ liệu mẩu',
+                    'Tự nhập dữ liệu']
+    
+    choice_input = st.sidebar.selectbox('Cách nhập dữ liệu', option_list)    
+    st.subheader(model_name)
+    if choice_input == 'Dữ liệu mẩu':
+        st.write('#### Sample dataset', pd_df)
+
+        # Chọn dữ liệu từ mẫu
+        selected_indices = st.multiselect('Chọn mẫu từ bảng dữ liệu:', pd_df.index)
+        selected_rows = pd_df.loc[selected_indices]
+        st.write('#### Kết quả')
+
+        if st.button('Dự đoán'):
+            if not selected_rows.empty:
+                X = selected_rows.iloc[:, :-1]
+                pred = prediction(X, model)
+
+                # Xuất ra màn hình
+                st.write("predict", pred)
+                results = pd.DataFrame({'Giá dự đoán': pred,
+                                            'Giá thực tế': selected_rows.TongGia})
+                st.write(results)
+            else:
+                st.error('Hãy chọn dữ liệu trước')
+
+    elif choice_input == 'Tự nhập dữ liệu':
+        with st.form("Nhập dữ liệu"):
+
+            feature1 = st.text_input("Feature 1")
+            feature2 = st.text_input("feature 2")
+            feature3 = st.text_input("Feature 3")
+
+            # Every form must have a submit button.
+            submitted = st.form_submit_button("Submit")
+            if submitted:
+                data_submitted = {'feature 1' : feature1,
+                                    'feature 2' : feature2,
+                                    'feature 3': feature3}
+                X = pd.DataFrame(data_submitted, index=[0])
+                pred = prediction(X, model)
+
+                # Xuất ra màn hình
+                st.write("predict", pred)
+                results = pd.DataFrame({'Giá dự đoán': pred,
+                                            'Giá thực tế': selected_rows.TongGia})
+                st.write(results)
+
+def create_dashboard(df):
+    st.subheader('Dashboard')
+
+    col1, col2 = st.columns(2)
+    col1.metric(label="Số lượng dự án", value=df.shape[0])
+    col2.metric(label="Giá tiền trung bình mỗi dự án", value=round(df['TongGia'].mean()))
+
+    fig1 = px.histogram(pd_df, x="Tinh", color="LoaiBDS", labels={
+                     "Tinh": "Tỉnh(Thành phố)",
+                     "LoaiBDS": "Loại BDS"
+                 },)
+    st.plotly_chart(fig1, use_container_width=True)
+    fig_col2, fig_col3 = st.columns(2)
+
+    fig2 = px.histogram(pd_df, x="LoaiBDS", y="TongGia", histfunc='avg', labels = {
+            "LoaiBDS": "Loại BDS",
+            "TongGia": "price"
+        })
+
+    pd_df2 = pd_df.groupby('LoaiBDS').size().reset_index(name='Observation')
+    fig3 = px.pie(pd_df2, values='Observation', names='LoaiBDS', title = 'Tỷ lệ các loại BDS')
+
+    fig_col2.plotly_chart(fig2)
+    fig_col3.plotly_chart(fig3)
+
+def main():
+    st.set_page_config(layout="wide")
+    st.title('Dự đoán giá bất động sản')
+    model_list = ['Dashboard',
+                    'Mô hình Linear Regression',
+                    'Mô hình Random Forest',
+                    'Mô hình Gradient Boosting',
+                    'Mô hình Decision Tree',
+                    'Mô hình Isotonic Regression',
+                    'Mô hình FMR']
+    choice_model = st.sidebar.selectbox('Mô hình huấn luyện trên:', model_list)
+
+
+    if choice_model =='Dashboard':
+        create_dashboard(pd_df)
+    elif choice_model == 'Mô hình Linear Regression':
+        model_page(choice_model, model_lr)
+
+    elif choice_model == 'Mô hình Random Forest':
+        model_page(choice_model, model_rf)
+
+    elif choice_model == 'Mô hình Gradient Boosting':
+        model_page(choice_model, model_gbt)
+
+    elif choice_model == 'Mô hình Decision Tree':
+        model_page(choice_model, model_dt)
+
+    elif choice_model == 'Mô hình Isotonic Regression':
+        model_page(choice_model, model_ir)
+
+    elif choice_model == 'Mô hình FMR':
+        model_page(choice_model, model_fmr)
+
+if __name__ == '__main__':
+    spark, sc = _initialize_spark()
+    ## Load dataset
+    df = spark.read.format('org.apache.spark.sql.json').load("./data/clean/clean.json")
+    data = df.drop(*['id','NgayDangBan', 'MoTa'])
+    #st.write("data ready")
+    data = data.fillna(0)
+    pd_df = data.toPandas()
+
+    ## Load model
+    model_lr = LinearRegressionModel.load("./model/linear_regression/lr_basic")
+    model_rf = RandomForestRegressionModel.load("./model/random_forest/rf_basic")
+    model_gbt = GBTRegressionModel.load("./model/gradient_boosted/gbt_basic")
+    model_dt = DecisionTreeRegressionModel.load("./model/decision_tree/dt_basic")
+    model_ir = IsotonicRegressionModel.load("./model/isotonic_regression/ir_basic")
+    model_fmr = FMRegressionModel.load("./model/factorization_machines_regression/fmr_basic")
+
+
+    main()
